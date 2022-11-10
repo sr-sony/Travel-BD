e1e93f1ba07420b1a977c48a85ab1b7513b00187
sr-sony <sr.sony.05@gmail.com>
2022-11-10 14:54:23 +0600

    navbar design

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 import { AuthContext } from "../../../context/AuthProvider/AuthProvider";
 
 const Header = () => {
-  const {user, logOut} = useContext(AuthContext);
-  const handleLogout=() => {
+  const { user, logOut } = useContext(AuthContext);
+  const handleLogout = () => {
     logOut()
-    .then(()=>{})
-    .catch(err => console.err(err))
-  }
+      .then(() => {})
+      .catch((err) => console.err(err));
+  };
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar bg-neutral text-neutral-content my-5">
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -31,45 +31,74 @@ const Header = () => {
           </label>
           <ul
             tabIndex={0}
-            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-neutral text-neutral-content rounded-box w-52"
           >
-            <li>
-              <Link to='/'>Home</Link>
+            <li className="font-semibold text-xl">
+              <Link to="/">Home</Link>
             </li>
-                
+
             <li>
-              <a>Item 3</a>
+              {user?.uid ? (
+                <>
+                  <span>{user?.displayName}</span>
+                  <button onClick={handleLogout}>Logout</button>
+                  <Link to="/additem">Add Item</Link>
+                  <Link to="/myreviews">My Reviews</Link>
+                </>
+              ) : (
+                <>
+                  <button>
+                    <Link to="/login">Login</Link>{" "}
+                  </button>
+                  <button>
+                    <Link to="/register">Registration</Link>{" "}
+                  </button>
+                </>
+              )}
             </li>
           </ul>
         </div>
-        <Link to='/' className="btn btn-ghost normal-case text-xl">Travel BD</Link>
+        <Link to="/" className="normal-case font-extrabold text-2xl ml-5">
+          Travel BD
+        </Link>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal p-0">
-          <li>
-          <Link to='/'>Home</Link>
+          <li className="font-semibold text-xl">
+            <Link to="/">Home</Link>
           </li>
-          {
-                  user?.uid?
-                  <>
-                  <span>{user?.displayName}</span>
-                  <button onClick={handleLogout}>Logout</button>
-                  <Link to='/additem'>Add Item</Link>
-                  <Link to='/myreviews'>My Reviews</Link>
-                  </>
-                  :
-                  <>
-                  <button><Link to='/login'>Login</Link> </button>
-                  <button><Link to='/register'>Registration</Link> </button>
-                  </>
-                }
-          <li>
-            <a>Item 3</a>
+          <li className="font-semibold text-xl">
+            {user?.uid ? (
+              <>
+                <Link className="mx-5" to="/additem">
+                  Add an Item
+                </Link>
+                <Link to="/myreviews">My Reviews</Link>
+              </>
+            ) : (
+              <></>
+            )}
           </li>
         </ul>
       </div>
       <div className="navbar-end">
-        <a className="btn">Login</a>
+        <li className="font-semibold text-xl">
+          {user?.uid ? (
+            <>
+              <span className="mx-5">{user?.displayName}</span>
+              <button onClick={handleLogout}>Logout</button>
+            </>
+          ) : (
+            <>
+              <button className="mx-5">
+                <Link to="/login">Login</Link>{" "}
+              </button>
+              <button className="mr-5">
+                <Link to="/register">Registration</Link>{" "}
+              </button>
+            </>
+          )}
+        </li>
       </div>
     </div>
   );
